9f9cff9436f3eb4ef92c07bb09a0d5b1e55485ea
JonnyBro <niktom.nikto@yandex.ru>
2021-12-04 16:52:30 +0500

    Алиасы команд в help

--- commands/Utility/help.js
+++ commands/Utility/help.js
@@ -7,17 +7,16 @@ module.exports = {
 	description: i18n.__("help.description"),
 	usage: "[опционально (команда)]",
 	async execute(client, message, args) {
-		const data = [];
-		const { commands } = client;
-
 		if (!args.length) return paginationEmbed(message, client.helpPages);
 
+		const data = [];
+		const { commands } = client;
 		const name = args[0].toLowerCase();
 		const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
 
 		if (!command) return message.channel.send(i18n.__("common.noCommandExists"));
 
-		data.push(`**${command.emoji || ":package:"} ${command.name}**`);
+		data.push(`${command.emoji || ":package:"} **${command.name}**`);
 		if (command.aliases) data.push(`**${i18n.__("help.aliases")}:** ${command.aliases.join(", ")}`);
 		if (command.description) data.push(`**${i18n.__("help.cmdDesc")}:** ${command.description}`);
 		if (command.usage) data.push(`**${i18n.__("help.usage")}:** **\`${client.prefix}${command.name} ${command.usage}\`**`);
